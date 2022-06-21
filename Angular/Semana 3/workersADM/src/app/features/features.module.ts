d4824f66225eeee72d9e250f32a6bac833ed4bea
rafaelxb <rfxbarbosa@gmail.com>
2022-06-21 20:35:33 -0300

    exercicio semana 3 angular

--- Angular/Semana 3/workersADM/src/app/features/features.module.ts
+++ Angular/Semana 3/workersADM/src/app/features/features.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListWorkersComponent } from './workers/list-workers/list-workers.component';
+import { ListWorkersComponent } from './components/workers/list-workers/list-workers.component';
 
 
 
